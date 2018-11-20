38fe79c2161936508c6013495cf30c3d30d3566f
Stephanie Mizelle <samizelle@gmail.com>
2018-11-20 15:34:56 -0500

    commit db name in web.config

---
+++ AMillionLittleThings/AMillionLittleThings/Models/Thing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AMillionLittleThings.Models
+{
+    public class Thing
+    {
+        public int NameID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+
+    public class ThingDbContext : DbContext
+    {
+        public DbSet<Thing> Things { get; set; }
+    }
+}
